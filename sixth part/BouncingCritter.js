9fd86cf32fb717f286993810fb1d30cb057c317c
Yoana_Boyanova <yoana.boyanova95@gmail.com>
2016-06-10 16:49:10 +0300

    initial commit

---
+++ sixth part/BouncingCritter.js
@@ -0,0 +1,9 @@
+function BouncingCritter() {
+    this.direction = randomElement(directionNames);
+};
+
+BouncingCritter.prototype.act = function(view) {
+    if (view.look(this.direction) != " ")
+        this.direction = view.find(" ") || "s";
+    return {type: "move", direction: this.direction};
+};
---
+++ sixth part/World.js
@@ -0,0 +1,67 @@
+function elementFromChar(legend, ch) {
+    if (ch == " ")
+        return null;
+    var element = new legend[ch]();
+    element.originChar = ch;
+    return element;
+}
+
+function World(map, legend) {
+    var grid = new Grid(map[0].length, map.length);
+    this.grid = grid;
+    this.legend = legend;
+
+    map.forEach(function(line, y) {
+        for (var x = 0; x < line.length; x++)
+            grid.set(new Vector(x, y),
+                elementFromChar(legend, line[x]));
+    });
+}
+
+function charFromElement(element) {
+    if (element == null)
+        return " ";
+    else
+        return element.originChar;
+}
+
+World.prototype.toString = function() {
+    var output = "";
+    for (var y = 0; y < this.grid.height; y++) {
+        for (var x = 0; x < this.grid.width; x++) {
+            var element = this.grid.get(new Vector(x, y));
+            output += charFromElement(element);
+        }
+        output += "\n";
+    }
+    return output;
+};
+
+World.prototype.turn = function() {
+    var acted = [];
+    this.grid.forEach(function(critter, vector) {
+        if (critter.act && acted.indexOf(critter) == -1) {
+            acted.push(critter);
+            this.letAct(critter, vector);
+        }
+    }, this);
+};
+
+World.prototype.letAct = function(critter, vector) {
+    var action = critter.act(new View(this, vector));
+    if (action && action.type == "move") {
+        var dest = this.checkDestination(action, vector);
+        if (dest && this.grid.get(dest) == null) {
+            this.grid.set(vector, null);
+            this.grid.set(dest, critter);
+        }
+    }
+};
+
+World.prototype.checkDestination = function(action, vector) {
+    if (directions.hasOwnProperty(action.direction)) {
+        var dest = vector.plus(directions[action.direction]);
+        if (this.grid.isInside(dest))
+            return dest;
+    }
+};
