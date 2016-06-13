c8a511b4d8b18c57974f1a9ce4736817b5da74f9
Yoana_Boyanova <yoana.boyanova95@gmail.com>
2016-06-13 12:09:37 +0300

    tiger added

--- sixth part/Tiger.js
+++ sixth part/Tiger.js
@@ -2,13 +2,13 @@ function Tiger(){
     this.energy = 30;
 }
 
-PlantEater.prototype.act = function(view) {
+Tiger.prototype.act = function(view) {
     var space = view.find(" ");
     if (this.energy > 60 && space)
         return {type: "reproduce", direction: space};
-    var plant = view.find("*");
-    if (plant && this.energy < 20)
-        return {type: "eat", direction: plant};
+    var animal = view.find("o");
+    if (animal && this.energy < 20)
+        return {type: "eat", direction: animal};
     if (space)
         return {type: "move", direction: space};
 };
